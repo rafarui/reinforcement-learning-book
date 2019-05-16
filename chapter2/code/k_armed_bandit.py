5018d7d422132a08aab2d0f910cc451b4e412424
Scott Brownlie <scottbrownlie1988@gmail.com>
2019-05-16 08:38:34 -0300

    put k-armed bandit in script and add non-stationary action values

---
+++ chapter2/code/k_armed_bandit.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 06:58:40 2019
+
+@author: Pichau
+"""
+
+import numpy as np
+
+
+class KArmedBandit:
+
+    def __init__(self, k_arms=10, alpha="1/n", epsilon=0., prior_estimation=0., true_reward=0.,
+                 init_deterministic=False, increment_mu=0., increment_sigma=0.):
+
+        """
+        :param k_arms: `int`
+            number of arms
+        :param alpha: `float` or "1/n"
+            step size
+        :param epsilon: `float` [0,1]
+            epsilon-greedy algorithm parameter. (probability of exploration)
+        :param prior_estimation: `float` or `list`
+            prior estimation for each action
+        :param true_reward: `float` or `list`
+            True reward for each arm.
+        :param init_deterministic: bool
+            if True, arm values with be initialised deterministically
+        :param increment_mu: `float`
+            mean of normally distributed increment to value functions on each 
+            time step for non-stationary problems
+        :param increment_sigma: `float`
+            standard deviation of normally distributed increment to value functions 
+            on each time step for non-stationary problems
+        """
+        self.k = k_arms
+        self.arms = np.arange(self.k)
+        self.time = 0
+        self.average_reward = 0
+        self.true_reward = true_reward
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.prior_estimation = prior_estimation
+        self.init_deterministic = init_deterministic
+        self.increment_mu = increment_mu
+        self.increment_sigma = increment_sigma
+
+        self.q_true = None  # true value for each arm
+        self.q_estimate = None  # the current estimate for the reward of each arm
+        self.action_count = None # how many times each arm was used.
+        self.best_action = None  # the best action amoung all the arms.
+
+    def initialize(self):
+        """
+        Initialize the process
+
+        :return: None
+        """
+        
+        if self.init_deterministic:
+            if np.isscalar(self.true_reward):
+                self.q_true = np.ones(self.k) * self.true_reward
+            else:
+                self.q_true = self.true_reward
+        else:
+            self.q_true = np.random.randn(self.k) + self.true_reward
+        self.q_estimate = np.zeros(self.k) + self.prior_estimation
+        self.action_count = np.zeros(self.k)
+        self.best_action = np.argmax(self.q_true)
+
+
+    def simulate(self, epochs=1000):
+        """
+        Simulate `epochs` times the K bandit arm.
+        
+        param: epochs
+            number of epochs.
+        
+        return: np.array shape (epochs,2)
+            array with the rewards received and if this action was the optimal action.
+        """
+        self.initialize()
+        action_rewards_best_action = np.zeros((epochs,2))
+        for t in range(epochs):
+            action = self.get_action()
+            reward = self.step(action)
+            #action_rewards_best_action[t,0] = action
+            action_rewards_best_action[t,0] = reward
+            if action == self.best_action:
+                action_rewards_best_action[t,1] = 1
+            self.increment_q()
+        return action_rewards_best_action
+    
+    def get_action(self):
+        """
+        Return an action
+        
+        :return: 
+            action
+        """
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.arms)
+
+        q_best = np.max(self.q_estimate)
+        return np.random.choice([action
+                                 for action, q in enumerate(self.q_estimate)
+                                 if q == q_best])
+
+    def get_reward(self, action, mu=0, sigma=1):
+
+        """
+
+        Get reward from an action
+
+        :param action:
+            arm to get the true reward
+        :param mu:
+            bias to add to the reward. (it might not make sense)
+        :param sigma:
+            variance of the noise to add to the reward.
+        :return: reward
+        """
+        return sigma * np.random.randn() + mu + self.q_true[action]
+
+
+    def step(self, action):
+        """
+            take an action, update estimation for this action
+
+        :param action:
+            arm to get the true reward
+        :return:
+            reward
+        """
+        # generate the reward
+        reward = self.get_reward(action)
+        # update time
+        self.time += 1
+        #compute the average reward
+        self.average_reward += (reward - self.average_reward) / self.time
+        #update the action count.
+        self.action_count[action] += 1
+        # update the estimate for the given action.
+        # TODO: add step size
+        if self.alpha == "1/n":
+            self.q_estimate[action] += 1.0 / self.action_count[action] * (reward - self.q_estimate[action])
+        else:
+            self.q_estimate[action] += self.alpha * (reward - self.q_estimate[action])
+
+        return reward
+    
+    def increment_q(self):
+        """
+        Modify value functions by adding guassian noise
+        """
+        self.q_true += (self.increment_sigma * np.random.randn(self.k) + self.increment_mu)
+        self.best_action = np.argmax(self.q_true)
